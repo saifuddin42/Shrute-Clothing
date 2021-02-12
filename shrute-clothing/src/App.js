055ee0b3111c6852e3ee37620c840da1902f9286
Saifuddin Telia <saifuddintelia@gmail.com>
2021-02-12 17:12:10 -0500

    Created Homepage Layout

--- shrute-clothing/src/App.js
+++ shrute-clothing/src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "./App.css";
 
-import { HomePage } from "./homepage.component";
+import HomePage from "./homepage.component";
+import "./homepage.styles.scss";
 
 function App() {
   return (
