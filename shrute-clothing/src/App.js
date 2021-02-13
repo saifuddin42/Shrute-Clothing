7ac86d2ffc2210d5592b17f89c07890fcd8ed4eb
Saifuddin Telia <saifuddintelia@gmail.com>
2021-02-12 22:45:27 -0500

    Updating the app with a ShopPage component, CollectionPreview component and CollectionItem component.

--- shrute-clothing/src/App.js
+++ shrute-clothing/src/App.js
@@ -4,18 +4,21 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import { Switch, Route, Link } from "react-router-dom";
 
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
+import ShopPage from "./pages/shop/shop.component.jsx";
+
+// const HatsPage = () => (
+//   <div>
+//     <h1>Hats Page</h1>
+//   </div>
+// );
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/shop/hats" component={HatsPage} />
+        {/* <Route path="/shop/hats" component={HatsPage} /> */}
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
