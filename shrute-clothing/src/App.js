4badb4dafdff6933641408c656a351bbe04f7207
Saifuddin Telia <saifuddintelia@gmail.com>
2021-02-13 00:05:56 -0500

    Added custom button, form, sign in components and a sign in sign up page component

--- shrute-clothing/src/App.js
+++ shrute-clothing/src/App.js
@@ -3,8 +3,9 @@ import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 import { Switch, Route, Link } from "react-router-dom";
-import ShopPage from "./pages/shop/shop.component.jsx";
+import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 // const HatsPage = () => (
 //   <div>
@@ -20,6 +21,7 @@ function App() {
         <Route exact path="/" component={HomePage} />
         {/* <Route path="/shop/hats" component={HatsPage} /> */}
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
