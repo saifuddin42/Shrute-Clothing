8674f7928c1b4f083bceb0e4493b70ffa0adfb40
Saifuddin Telia <saifuddintelia@gmail.com>
2021-02-12 23:04:46 -0500

    Added Header component and links to home and shop page

--- shrute-clothing/src/App.js
+++ shrute-clothing/src/App.js
@@ -3,8 +3,8 @@ import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 import { Switch, Route, Link } from "react-router-dom";
-
 import ShopPage from "./pages/shop/shop.component.jsx";
+import Header from "./components/header/header.component";
 
 // const HatsPage = () => (
 //   <div>
@@ -15,6 +15,7 @@ import ShopPage from "./pages/shop/shop.component.jsx";
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         {/* <Route path="/shop/hats" component={HatsPage} /> */}
